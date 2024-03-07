59d05a7aff5ba7cc237843aad1680c5bc976037a
Zalient <Zacharious@icloud.com>
2024-03-07 10:27:37 +0000

    Added the project code

---
+++ src/main/java/uk/ac/soton/comp1206/App.java
@@ -0,0 +1,16 @@
+package uk.ac.soton.comp1206;
+
+/**
+ * App Class
+ */
+public class App 
+{
+    /**
+     * Hello World main method
+     * @param args
+     */
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
